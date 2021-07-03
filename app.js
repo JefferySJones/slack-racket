981320452b12d2948d7a67d20ea400eb99c51cbf
JefferySJones <jeffery.jones@lendio.com>
2021-07-02 18:17:10 -0600

    print out directories and files

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { App } = require('@slack/bolt');
-const sound = require("sound-play");
+const sound = require('sound-play');
+const fs = require('fs');
+const path = require('path');
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
@@ -23,8 +25,24 @@ const reply = async (cb, msg) => {
 // Whitelist
 // Fuzzy Search + Response with similar sounds
 
+const getDirectories = srcPath => {
+    try {
+        return fs.readdirSync(srcPath).filter(file => fs.statSync(path.join(srcPath, file)).isDirectory());
+    } catch {
+        return [ `Path does not exist: ${srcPath}` ];
+    }
+}
+const getFiles = srcPath => {
+    try {
+        return fs.readdirSync(srcPath).filter(file => !fs.statSync(path.join(srcPath, file)).isDirectory());
+    } catch {
+        return [ `Path does not exist: ${srcPath}` ];
+    }
+}
+    
 (async () => {
-    const playRegex = /^(play)\s+(.*)/;
+    const playRegex = /^(play)\s+([^\s]*)/;
+    const listRegex = /^(play list|play folders)(\s+|)(.*)/;
 
     await app.start(process.env.PORT || 3009);
 
@@ -33,12 +51,33 @@ const reply = async (cb, msg) => {
         sound.play("sounds/kungfury/knockles.mp3");
     })
 
-    app.message('help', async ({ message, say }) => {
+    app.message('play help', async ({ message, say }) => {
         await reply(say, "```Commands: \n play <path to sound> \n knock knock```");
+    })
+
+    app.message(listRegex, async ({ message, say }) => {
+        const match = message.text.match(listRegex);
+        const folder = match[3];
+        if (!folder) {
+            // Reply with directories
+            await reply(say, "```" + JSON.stringify(
+                    getDirectories('./sounds')
+                ).replace(/\,/g, '\n').replace(/\[|\]|\"/g, '') + "```");
+        } else {
+            // Reply with the files
+            await reply(say, "```" + JSON.stringify(
+                    getFiles('./sounds/' + folder).map((file) => folder + '/' + file.replace('.mp3', ''))
+                ).replace(/\,/g, '\n').replace(/\[|\]|\"/g, '') + "```");
+        }
+        
     })
 
     app.message(playRegex, async ({ message, say }) => {
         const match = message.text.match(playRegex);
+        const file = match[2];
+        if (!file || ['help', 'list'].includes(file)) {
+            return;
+        }
         await reply(say, `Attempting to play sounds/${match[2]}.mp3`);
         sound.play(`sounds/${match[2]}.mp3`);
     })
